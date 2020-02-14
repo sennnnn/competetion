15ae3e86e6033c7ea52bf1d34881e26374fa5d8c
sen <sen@struggle.com>
2020-02-14 19:31:37 +0800

    我的美赛源码仓库初始化

---
+++ statistics.py
@@ -0,0 +1,124 @@
+import csv
+
+from util import csv_process,team_member_get,team_key_word_extract,cooperation_detect
+
+fullevents_path = 'data/fullevents.csv'
+matches_path = 'data/matches.csv'
+passingevents_path = 'data/passingevents.csv'
+
+csv_file = csv.reader(open(passingevents_path, 'r'))
+
+all_info = list(csv_file)
+
+team_name = 'Husk'
+
+result_path = 'result'
+
+# 总共有这么些数据项目
+# ['MatchID', 'TeamID', 'OriginPlayerID', 'DestinationPlayerID', 'MatchPeriod', 'EventTime', \
+# 'EventSubType', 'EventOrigin_x', 'EventOrigin_y', 'EventDestination_x', 'EventDestination_y']
+
+all_info = csv_process(all_info)
+
+# 队伍综合能力
+# 队伍传球能力：累计传球次数
+# 队伍配合能力：累计配合次数，二人配合 + 三人配合
+# 小队配合能力：二人配合次数，三人配合次数
+# 个人配合能力：传球次数，接球次数
+
+team_key_word_list = ['Husk', 'Oppo']
+team_pass_count = {'Husk':0, 'Oppo':0}
+team_cooperation_count = {'Husk':{2:0, 3:0}, 'Oppo':{2:0, 3:0}}
+team_cooperation_list = {'Husk':[], 'Oppo':[]}
+
+
+for i in range(len(all_info)):
+    # 统计传球次数
+    Origin_player = all_info[i]['OriginPlayerID']
+    team_key_word = team_key_word_extract(Origin_player)
+    team_pass_count[team_key_word] += 1
+    if(i == 0):
+        continue
+    # 统计合作次数
+    info_1 = all_info[i-1]
+    info_2 = all_info[i]
+    players,flag = cooperation_detect(info_1, info_2)
+    if(flag):
+        team_cooperation_list[team_key_word].append((i+1, tuple(players)))
+        team_cooperation_count[team_key_word][len(players)] += 1
+
+print(team_pass_count)
+print(team_cooperation_count)
+
+f = open('{}/cooperation&pass_count.txt'.format(result_path), 'w', encoding='utf-8')
+f.write('p.s. 注意 Oppo 是敌队的意思。\n传球次数统计：\n{}队:{}  {}队:{}\n\n'.format('Husk', team_pass_count['Husk'], \
+                                                                                'Oppo', team_pass_count['Oppo']))
+
+f.write('配合次数统计：\n{}队：二元配合：{} 三元配合：{}  {}队：二元配合：{} 三元配合{}\n\n'\
+                            .format('Husk', team_cooperation_count['Husk'][2], team_cooperation_count['Husk'][3], \
+                                    'Oppo', team_cooperation_count['Oppo'][2], team_cooperation_count['Oppo'][3]))
+f.write('!!! 以下是配合明细 !!!\n================================================================================\n')
+
+two_player_cooperation_list = []
+tri_player_cooperation_list = []
+for one in team_cooperation_list[team_name]:
+    raw_line_number = one[0]
+    players = list(one[1])
+    cooperation_player_number = len(players)
+    if(cooperation_player_number == 2):
+        players.append(players[0])
+        two_player_cooperation_list.append((raw_line_number, cooperation_player_number, tuple(players)))
+        continue
+    tri_player_cooperation_list.append((raw_line_number, cooperation_player_number, tuple(players)))
+
+[f.write("raw_line_number:{} cooperation_player_number:{} {}==>{}==>{}\n".format(a, b, *c)) \
+    for a,b,c in two_player_cooperation_list]
+
+[f.write("raw_line_number:{} cooperation_player_number:{} {}==>{}==>{}\n".format(a, b, *c)) \
+    for a,b,c in tri_player_cooperation_list]
+
+f.close()
+
+
+f = open('{}/pass_origin.txt'.format(result_path), 'w', encoding='utf-8')
+f_ = open('{}/pass_dest.txt'.format(result_path), 'w', encoding='utf-8')
+
+f.write('这个是传出的记录!\n\n========================================================================\n')
+f_.write('这个是接住的记录!\n\n========================================================================\n')
+
+OriginPlayerID_all = [x['OriginPlayerID'] for x in all_info]
+DesinationPlayerID_all = [x['DestinationPlayerID'] for x in all_info]
+
+team_members = team_member_get(OriginPlayerID_all, DesinationPlayerID_all, team_name)
+
+# 传球统计
+playerwise_pass_origin_count = {member:{member_:0 for member_ in team_members} for member in team_members}
+# 接球统计
+playerwise_pass_dest_count  = {member:{member_:0 for member_ in team_members} for member in team_members}
+for member in team_members:
+    playerwise_pass_origin_count[member]['all'] = 0
+    playerwise_pass_dest_count[member]['all'] = 0
+
+for i,j in zip(OriginPlayerID_all, DesinationPlayerID_all):
+    if(team_name not in i or team_name not in j):
+        continue
+
+    # 传出
+    playerwise_pass_origin_count[i][j] += 1
+    playerwise_pass_origin_count[i]['all'] += 1
+    # 接到
+    playerwise_pass_dest_count[j][i] += 1
+    playerwise_pass_dest_count[j]['all'] += 1
+
+for member in team_members:
+    f.write('[{}]\n'.format(member))
+    f_.write('[{}]\n'.format(member))
+    member_dict = playerwise_pass_origin_count[member]
+    member_dict_ = playerwise_pass_dest_count[member]
+    for i,j in zip(member_dict.keys(), member_dict_.keys()):
+        f.write('{}:{}\n'.format(i, member_dict[i]))
+        f_.write('{}:{}\n'.format(j, member_dict_[j]))
+    f.write('\n')
+    f_.write('\n')
+f.close()
+f_.close()
---
+++ util.py
@@ -0,0 +1,106 @@
+
+def csv_process(csv_list):
+    item_list = csv_list[0]
+    ret_list = []
+    csv_list = csv_list[1:]
+    for one_line in csv_list:
+        ret_list.append({x:y for x,y in zip(item_list, one_line)})
+    
+    return ret_list
+
+def ret_from_player_ID(ID, int_max):
+    string = ID.split('_')[1]
+    char_1,char_2 = string[0],string[1:]
+    temp = 0
+    if(char_1 == 'D'):
+        temp = 0
+    elif(char_1 == 'F'):
+        temp = int_max
+    elif(char_1 == 'M'):
+        temp = int_max*2
+    elif(char_1 == 'G'):
+        temp = int_max*3
+    else:
+        temp = int_max*4
+    
+    return temp + int(char_2)
+
+def team_member_get(OriginPlayerID_all, DestinationPlayerID_all, team_key_word):
+    """
+    获取某个队伍的球员清单。
+    Args:
+        OriginPlayerID_all:所有传球队员记录。
+        DestinationPlayerID_all:所有接球队员记录。
+        team_key_word:球队的关键字。
+    Return:
+        team_member_list:球队的成员列表，球员一般用 XXteam_XX 表示，'_' 后面的是球员的标识，
+        为一个字母和一个数字组合，该列表进行过排序球员首先按字母 D，F，M，G 排序，然后再安装数字排序
+    """
+    members = []
+    for i,j in zip(OriginPlayerID_all, DestinationPlayerID_all):
+        # 判断这个球员是否为该队伍。
+        if(team_key_word not in i or team_key_word not in j):
+            continue
+        if(i not in members):
+            members.append(i)
+        if(j not in members):
+            members.append(j)
+    
+    member_count = len(members)
+    members = sorted(members, key=lambda x : ret_from_player_ID(x, member_count))
+
+    return members
+
+def team_key_word_extract(ID):
+    return ID.split('_')[0][:4]
+
+def pass_side_judge(info, team_key_word):
+    """
+    判断传接球是否是选定方，我方关键词：Husk 敌方关键词：Oppo。
+    Args:
+        info:判断的数据条目。
+    Return:
+        bool:是否。
+    """
+    Origin = info['OriginPlayerID']
+    Dest = info['DestinationPlayerID']
+    if(team_key_word not in Origin or team_key_word not in Dest):
+        return False
+    
+    return True
+
+def cooperation_detect(info_1, info_2):
+    """
+    通过输入三个信息条目，来检测是否构成一次配合。
+    Args:
+        info_1, info_2:两条条目。
+    Return:
+        players:构成配合的 player_name 列表。
+        flag:是否构成配合。
+    """
+    players = []
+    time_accu = 0
+    Origin_1 = info_1['OriginPlayerID']
+    Origin_2 = info_2['OriginPlayerID']
+    Dest_1 = info_1['DestinationPlayerID']
+    Dest_2 = info_2['DestinationPlayerID']
+    time_stamp_1 = float(info_1['EventTime'])
+    time_stamp_2 = float(info_2['EventTime'])
+    # 第一次传球与第二次传球判断
+    if((time_stamp_1-time_stamp_2) > 5):
+        return players, False
+    else:
+        if(team_key_word_extract(Origin_1) != team_key_word_extract(Origin_2)):
+            return players, False
+        else:
+            if(Dest_1 != Origin_2 or Origin_1 == Dest_1 or Origin_2 == Dest_2):
+                return players, False
+            else:
+                players.append(Origin_1)
+                players.append(Dest_1)
+                players.append(Origin_2)
+                players.append(Dest_2)
+                temp = list(set(players))
+                players = sorted(temp, key=players.index)
+                return players, True
+
